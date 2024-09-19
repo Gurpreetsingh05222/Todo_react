16f1ab47dd751a0ea8ce8313d6ccd532111861f6
gurpreetsingh05222 <gurpreetsingh05222@gmail.com>
2024-09-18 23:03:42 -0400

    TodoList class component

---
+++ src/TodoList.js
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+
+class TodoList extends Component{
+    constructor(props){
+        super(props);
+        this.state = { todos: [] };
+    }
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default TodoList;
