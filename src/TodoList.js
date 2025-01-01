c3c6db442ee79fc2a3004927cd64e869727230d8
gurpreetsingh05222 <gurpreetsingh05222@gmail.com>
2025-01-01 16:51:09 -0500

    new todo form

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,24 +1,32 @@
 import React, { Component } from "react";
-import Todo from './Todo';
+import Todo from "./Todo";
+import NewTodoForm from "./NewTodoForm";
 
 class TodoList extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            todos: [{task: "Walk the Dog"}, {task: "Groom the Dog"}] 
-        };
-    }
-    render() {
-        const todos = this.state.todos.map(todo => {
-            return <Todo task={todo.task} />;
-        });
-        return (
-            <div>
-                <h1>Todo List!</h1>
-                <ul>{todos}</ul>
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [{ task: "Walk the Dog" }, { task: "Groom the Dog" }],
+    };
+    this.create = this.create.bind(this);
+  }
+  create(newTodo) {
+    this.setState({
+      todos: [...this.state.todos, newTodo],
+    });
+  }
+  render() {
+    const todos = this.state.todos.map((todo) => {
+      return <Todo task={todo.task} />;
+    });
+    return (
+      <div>
+        <h1>Todo List!</h1>
+        <NewTodoForm createTodo={this.create} />
+        <ul>{todos}</ul>
+      </div>
+    );
+  }
 }
 
 export default TodoList;
